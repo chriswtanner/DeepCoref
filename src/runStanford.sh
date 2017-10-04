a230a7c05e972870dbdb4757193ce228e3c60418
chriswtanner <christanner@cs.brown.edu>
2017-10-04 13:32:13 -0700

    ccnn

--- src/runStanford.sh
+++ src/runStanford.sh
@@ -9,7 +9,7 @@ if [ ${me} = "ctanner" ]
 then
     echo "ON THE GRID!"
     baseDir=${gridDir}
-    ./home/jrasley/set_cuda8_cudnn6.sh
+    #./home/jrasley/set_cuda8_cudnn6.sh
     export CUDA_HOME=/contrib/projects/cuda8.0
     export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
     export PATH=${CUDA_HOME}/bin:${PATH}
