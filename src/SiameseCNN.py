a4ae6b99f096c3cb4303426b6a64d7e0e517e99d
chriswtanner <christanner@cs.brown.edu>
2017-10-05 13:12:17 -0700

    f1

--- src/SiameseCNN.py
+++ src/SiameseCNN.py
@@ -138,7 +138,7 @@ class SiameseCNN:
 
     # from a list of predictions, find the optimal f1 point
     def compute_optimal_f1(self, dmPairs, predictions, golds):
-        print("* in compute_optimal_f1()")
+        print("* in compute_optimal_f1!!!()")
         # sorts the predictions from smallest to largest
         # (where smallest means most likely a pair)
         predToIndices = defaultdict(list)
