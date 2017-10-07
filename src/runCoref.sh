48c17d59492f034e13bc1529963160d43d3ba0ef
chriswtanner <christanner@cs.brown.edu>
2017-10-07 12:22:14 -0700

    g

--- src/runCoref.sh
+++ src/runCoref.sh
@@ -26,8 +26,8 @@ then
 		echo ${LD_LIBRARY_PATH}
 	else
 		echo "*   ON THE GRID!"
-		#export CUDA_HOME=/usr
-		export CUDA_HOME=/contrib/projects/cuda8.0
+		export CUDA_HOME=/usr
+		#export CUDA_HOME=/contrib/projects/cuda8.0
 		export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 		export PATH=${CUDA_HOME}/bin:${PATH}
     	echo ${CUDA_HOME}
