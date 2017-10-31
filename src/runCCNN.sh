35d64fe8e9cd2961a6b8651e32d1a87c29bfec49
chriswtanner <christanner@cs.brown.edu>
2017-10-30 23:44:21 -0400

    a

--- src/runCCNN.sh
+++ src/runCCNN.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 cd /home/christanner/researchcode/DeepCoref/src/
-numLayers=(1 2 3) 
-numEpochs=(2 3 10 20)
-windowSize=(1 2 3 5)
-numNeg=(1 3 5 10)
-batchSize=(64 128 256)
+numLayers=(1) # 2 3) 
+numEpochs=(2) # 3 10 20)
+windowSize=(1) # 2 3 5)
+numNeg=(1) # 3 5 10)
+batchSize=(64) # 128 256)
 shuffle=(f) # t)
 embSize=(400) # 50
-dropout=(0.0 0.2 0.3 0.4)
+dropout=(0.0) # 0.2 0.3 0.4)
 hddcrp="predict"
-clusterMethod=("min" "avg" "avgavg")
+clusterMethod=("min") # "avg" "avgavg")
 source ~/researchcode/DeepCoref/venv/bin/activate
 # source ~/researchcode/DeepCoref/oldcpu/bin/activate
 # source /data/people/christanner/tfcpu/bin/activate
 
+			    # qsub -pe smp 32 -l vlong -o cpu32b_nl${nl}_ne${ne}_ws${ws}_neg${neg}_bs${bs}_s${s}.out runCoref.sh cpu ${nl} ${ne} ${ws} ${neg} ${bs} ${s} ${emb}
 for nl in "${numLayers[@]}"
 do
-    for ne in "${numEpochs[@]}"
-    do
-	for ws in "${windowSize[@]}"
+	for ne in "${numEpochs[@]}"
 	do
-	    for neg in "${numNeg[@]}"
-	    do
-		for bs in "${batchSize[@]}"
+		for ws in "${windowSize[@]}"
 		do
-		    for s in "${shuffle[@]}"
-		    do
-			for emb in "${embSize[@]}"
+			for neg in "${numNeg[@]}"
 			do
-			    # qsub -pe smp 32 -l vlong -o cpu32b_nl${nl}_ne${ne}_ws${ws}_neg${neg}_bs${bs}_s${s}.out runCoref.sh cpu ${nl} ${ne} ${ws} ${neg} ${bs} ${s} ${emb}
-			    a=1
+				for bs in "${batchSize[@]}"
+				do
+					for s in "${shuffle[@]}"
+					do
+						for emb in "${embSize[@]}"
+						do
+							for dr in "${dropout[@]}"
+							do
+								for cm in "${clusterMethod[@]}"
+								do
+									qsub -pe smp 8 -l vlong -o cpuGOLD_nl${nl}_ne${ne}_ws${ws}_neg${neg}_bs${bs}_s${s}_e${emb}_dr${dr}_cm${cm}.out runCoref.sh FULL gpu ${nl} ${ne} ${ws} ${neg} ${bs} ${s} ${emb} ${hddcrp} ${dr} ${cm}
+								done
+							done
+						done
+					done
+				done
 			done
-		    done
 		done
-	    done
 	done
-    done
 done
 # exit 1
 # GPU
