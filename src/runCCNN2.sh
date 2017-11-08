5bac6de49690e52131ba83a58860a91ccd0a6cde
chriswtanner <christanner@cs.brown.edu>
2017-11-08 18:45:33 -0500

    sp's

--- src/runCCNN2.sh
+++ src/runCCNN2.sh
@@ -8,7 +8,7 @@ hn=`hostname`
 baseDir="/Users/christanner/research/DeepCoref/"
 brownDir="/home/ctanner/researchcode/DeepCoref/"
 
-stoppingPoints=(0.26) # 0.28 0.301 0.32 0.34 0.37 0.39 0.401 0.41 0.42 0.43 0.44 0.45 0.46 0.47 0.48 0.49 0.501 0.51 0.52 0.53 0.55 0.57 0.601)
+stoppingPoints=(0.26 0.28 0.301 0.32 0.34 0.37 0.39 0.401 0.41 0.42 0.43 0.44 0.45 0.46 0.47 0.48 0.49 0.501 0.51 0.52 0.53 0.55 0.57 0.601)
 
 if [ ${me} = "ctanner" ]
 then
