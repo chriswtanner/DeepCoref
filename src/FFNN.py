261295bbf53582a1a74c8bc0e50d390739f8c940
chriswtanner <christanner@cs.brown.edu>
2017-11-28 19:32:01 -0500

    2

--- src/FFNN.py
+++ src/FFNN.py
@@ -275,7 +275,7 @@ class FFNN:
 							print("* ERROR: dm1,dm2 or reverse aren't in predictions")
 							exit(1)
 
-						curX += dmToLinkDistribution[dm1]
+						curX += docToListDistribution[doc]#dmToLinkDistribution[dm1]
 
 						curY = []
 						if isHDDCRP:
