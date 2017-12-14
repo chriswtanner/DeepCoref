3b5ca75700b38347ad6361893453385aee86f14a
chriswtanner <christanner@cs.brown.edu>
2017-12-13 23:01:57 -0500

    asdf

--- src/runCCNN2.sh
+++ src/runCCNN2.sh
@@ -10,7 +10,7 @@ brownDir="/home/ctanner/researchcode/DeepCoref/"
 
 #stoppingPoints=(0.401 0.41 0.42 0.43 0.44 0.45 0.46 0.47 0.48 0.49 0.501 0.51 0.52 0.53 0.55 0.57 0.601)
 #stoppingPoints=(0.51)
-stoppingPoints=(0.701 0.72 0.74 0.76 0.78 0.801 0.81 0.83 0.85 0.87 0.89 0.91)
+stoppingPoints=(0.701 0.72 0.74 0.76 0.78 0.801 0.81 0.83 0.85 0.87 0.88 0.89 0.901 0.91)
 #stoppingPoints=(0.24 0.27 0.301 0.33 0.36 0.39 0.401 0.41 0.42 0.43 0.44 0.45 0.46 0.47 0.48 0.49 0.501 0.51 0.52 0.53 0.55 0.57 0.601 0.62 0.66 0.701 0.72 0.74 0.76 0.78 0.801 0.81)
 #stoppingPoints=(0.12 0.15 0.18 0.21 0.24 0.27 0.301 0.33 0.36 0.39 0.42 0.45 0.47 0.48 0.49 0.501 0.51 0.52 0.53 0.55 0.57 0.601 0.62 0.66 0.701 0.72 0.74 0.76 0.78 0.801 0.81)
 if [ ${me} = "ctanner" ]
@@ -191,7 +191,7 @@ if [ "$useECBTest" = false ] ; then
 		ceafe=`./scorer.pl ceafe ${goldFile} ${f} | grep "Coreference: Recall" | cut -d" " -f 11 | sed 's/.$//'`
 		sum=`echo ${muc}+${bcub}+${ceafe} | bc`
 		avg=`echo "scale=2;$sum/3.0" | bc`
-		echo "CoNLLF1:" ${f} ${avg}
+		echo "CoNLLF1:" ${f} ${avg} "OTHERS:" ${muc} ${bcub} ${ceafe}
 		rm -rf ${f}
 	done
 fi
