9d647423767a6ced23016294928e5719645bd0d3
chriswtanner <christanner@cs.brown.edu>
2018-02-13 11:08:46 -0500

    hddcrp

--- src/runCCNN2.sh
+++ src/runCCNN2.sh
@@ -8,7 +8,7 @@ hn=`hostname`
 baseDir="/Users/christanner/research/DeepCoref/"
 brownDir="/home/ctanner/researchcode/DeepCoref/"
 
-stoppingPoints=(0.401,0.501,0.601)
+stoppingPoints=(0.501)
 # (0.301,0.401,0.501,0.601)
 # ()
 # 0.15,0.201,0.25,0.275,0.301,0.325,0.35,0.375,0.401,0.425,0.45,0.475,0.501,0.525,0.55,0.575,0.601,0.65,0.701)
@@ -66,7 +66,7 @@ stoplistFile=${baseDir}"data/stopwords.txt"
 mentionsFile=${baseDir}"data/goldTruth_events.txt"
 embeddingsFile=${gloveOutput}
 embeddingsType="type"
-useECBTest=true
+useECBTest=false
 device=$2
 numLayers=$3
 poolType=$4
