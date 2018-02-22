2c902b59588e77503daea832c774488c81cd12d0
chriswtanner <christanner@cs.brown.edu>
2018-02-22 16:40:08 -0500

    asdf

--- src/runCCNN2.sh
+++ src/runCCNN2.sh
@@ -8,7 +8,7 @@ hn=`hostname`
 baseDir="/Users/christanner/research/DeepCoref/"
 brownDir="/home/ctanner/researchcode/DeepCoref/"
 
-stoppingPoints=(0.45 0.475 0.501 0.525 0.55)
+stoppingPoints=(0.35 0.375 0.401 0.425 0.45 0.475 0.501 0.525)
 stoppingPoints2=(0.25 0.30 0.35 0.401 0.45 0.475 0.501 0.525 0.55 0.601 0.65 0.701 0.75)
 
 if [ ${me} = "ctanner" ]
